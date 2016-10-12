c32c8422f5efd051724bc2768c6cce9c20eb2b3f
awecea <awecea@gmail.com>
2016-10-12 21:16:03 +0300

    add step 5 big task level 28

--- src/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -1,12 +1,16 @@
 package com.javarush.test.level28.lesson15.big01;
 
 import com.javarush.test.level28.lesson15.big01.model.Provider;
+import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
 
     private Provider[] providers;
+    private List<Vacancy> vacancies = new ArrayList<>();
 
     public Controller(Provider... providers) {
         if (providers.length == 0) {
@@ -20,5 +24,15 @@ public class Controller {
         return "Controller{" +
                 "providers=" + Arrays.toString(providers) +
                 '}';
+    }
+
+    public void scan() {
+        for (Provider provider : providers) {
+            List<Vacancy> vacs = provider.getJavaVacancies("java Киев");
+            if (vacs != null) {
+                vacancies.addAll(vacs);
+            }
+        }
+        System.out.println(vacancies.size());
     }
 }
