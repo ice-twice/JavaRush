bacb09887db462749aff950bc6a993731eb244ac
awecea <awecea@gmail.com>
2016-10-13 18:45:53 +0300

    add step 12 big task level 28

--- src/com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ src/com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,7 +1,18 @@
 package com.javarush.test.level28.lesson15.big01;
 
+import com.javarush.test.level28.lesson15.big01.model.HHStrategy;
+import com.javarush.test.level28.lesson15.big01.model.Model;
+import com.javarush.test.level28.lesson15.big01.model.Provider;
+import com.javarush.test.level28.lesson15.big01.view.HtmlView;
+
 public class Aggregator {
     public static void main(String[] args) {
+        Provider provider = new Provider(new HHStrategy());
+        HtmlView htmlView = new HtmlView();
+        Model model = new Model(htmlView, provider);
+        Controller controller = new Controller(model);
+        htmlView.setController(controller);
+        htmlView.userCitySelectEmulationMethod();
     }
 }
 
--- src/com/javarush/test/level28/lesson15/big01/model/Model.java
+++ src/com/javarush/test/level28/lesson15/big01/model/Model.java
@@ -10,7 +10,7 @@ public class Model {
     private View view;
     private Provider[] providers;
 
-    public Model(View view, Provider[] providers) {
+    public Model(View view, Provider... providers) {
         if (view == null || providers == null || providers.length == 0) {
             throw new IllegalArgumentException();
         }
