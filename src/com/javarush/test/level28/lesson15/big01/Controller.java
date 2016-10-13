eff590af6602be8c81685f551bad5d96c64b6758
awecea <awecea@gmail.com>
2016-10-13 18:31:15 +0300

    add step 11 big task level 28

--- src/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -1,38 +1,24 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.Provider;
+import com.javarush.test.level28.lesson15.big01.model.Model;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
 
-    private Provider[] providers;
     private List<Vacancy> vacancies = new ArrayList<>();
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers.length == 0) {
+    public Controller(Model model) {
+        if (model == null) {
             throw new IllegalArgumentException();
         }
-        this.providers = providers;
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        for (Provider provider : providers) {
-            List<Vacancy> vacs = provider.getJavaVacancies("киев");
-            if (vacs != null) {
-                vacancies.addAll(vacs);
-            }
-        }
-        System.out.println(vacancies.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
