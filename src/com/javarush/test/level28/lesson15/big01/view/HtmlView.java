dda48b84d56b42f072d490152877288bf21b75ab
awecea <awecea@gmail.com>
2016-10-13 19:26:46 +0300

    add step 13 big task level 28

--- src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
+++ src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
@@ -6,6 +6,7 @@ import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
 import java.util.List;
 
 public class HtmlView implements View {
+    private final String filePath = "./src/" + this.getClass().getPackage().getName().replace(".", "/") + "/vacancies.html";
     private Controller controller;
 
     @Override
@@ -21,5 +22,13 @@ public class HtmlView implements View {
 
     public void userCitySelectEmulationMethod() {
         controller.onCitySelect("Odessa");
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancies) {
+        return null;
+    }
+
+    private void updateFile(String s) {
+
     }
 }
