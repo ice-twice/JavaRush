0a09545d6af9f70ae3eadfb84ccfbda976e19ad4
awecea <awecea@gmail.com>
2016-10-14 13:29:49 +0300

    add step 15 big task level 28

--- src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
+++ src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
@@ -2,8 +2,13 @@ package com.javarush.test.level28.lesson15.big01.view;
 
 import com.javarush.test.level28.lesson15.big01.Controller;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
@@ -13,8 +18,8 @@ public class HtmlView implements View {
 
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
-
+        String resultHtml = getUpdatedFileContent(vacancies);
+        updateFile(resultHtml);
     }
 
     @Override
@@ -27,14 +32,66 @@ public class HtmlView implements View {
     }
 
     private String getUpdatedFileContent(List<Vacancy> vacancies) {
-        return null;
+        Document doc;
+        try {
+            doc = getDocument();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
+        Element tempElement = doc.select("[class=vacancy template]").first();
+        Element templateVacancy = tempElement.clone();
+        templateVacancy.removeClass("template").removeAttr("style");
+        doc.getElementsByAttributeValue("class", "vacancy").remove();
+
+        for (Vacancy v : vacancies) {
+            String title = v.getTitle();
+            String url = v.getUrl();
+            String city = v.getCity();
+            String company = v.getCompanyName();
+            String salary = v.getSalary();
+
+            Element newVacancy = templateVacancy.clone();
+
+            Element titleElem = newVacancy.select("[class=title]").select("a").first();
+            titleElem.attr("href", url);
+            titleElem.text(title);
+
+            Element cityElem = newVacancy.select("[class=city]").first();
+            cityElem.text(city);
+
+            Element companyElem = newVacancy.select("[class=companyName]").first();
+            companyElem.text(company);
+
+            Element salaryElem = newVacancy.select("[class=salary]").first();
+            salaryElem.text(salary);
+
+            tempElement.before(newVacancy);
+        }
+
+        return doc.outerHtml();
     }
 
     private void updateFile(String s) {
-        try (PrintWriter printWriter = new PrintWriter(filePath)) {
+        PrintWriter printWriter = null;
+        try {
+            printWriter = new PrintWriter(filePath);
             printWriter.print(s);
         } catch (FileNotFoundException e) {
-
+        } finally {
+            if (printWriter != null) {
+                printWriter.close();
+            }
         }
+        // in this way does not pass test
+        /*try (PrintWriter printWriter = new PrintWriter(filePath)) {
+            printWriter.print(s);
+        } catch (FileNotFoundException e) {
+        }*/
+    }
+
+    protected Document getDocument() throws IOException {
+        File file = new File(filePath);
+        return Jsoup.parse(file, "UTF-8");
     }
 }
