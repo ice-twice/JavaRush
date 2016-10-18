71f086c335aaa5dfdb4f7096e040fb2bf9407833
awecea <awecea@gmail.com>
2016-10-18 15:36:42 +0300

    add step 14 big task level 29

--- src/com/javarush/test/level29/lesson15/big01/user/User.java
+++ src/com/javarush/test/level29/lesson15/big01/user/User.java
@@ -4,12 +4,9 @@ public class User {
     private String name;
     private String surname;
     private int age;
-
-    private String country;
-    private String city;
-    private House house;
-
+    private boolean man;
     private Work work;
+    private Address address;
 
     public User(String name, String surname, int age) {
         this.name = name;
@@ -42,23 +39,23 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -67,6 +64,14 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
     }
 
     public void printInfo() {
@@ -79,5 +84,9 @@ public class User {
             System.out.println("Пользователь моложе 16 лет");
         else
             System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss() {
+        return work.getBoss();
     }
 }
