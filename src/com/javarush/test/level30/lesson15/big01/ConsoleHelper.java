b1ff1b89d8ee00275198efc7c3893517e3baa1da
awecea <awecea@gmail.com>
2016-10-20 15:19:34 +0300

    add step 2 big task level 30

--- src/com/javarush/test/level30/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level30/lesson15/big01/ConsoleHelper.java
@@ -1,4 +1,38 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String lineFromConsole = null;
+        while (lineFromConsole == null) {
+            try {
+                lineFromConsole = reader.readLine();
+            } catch (IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return lineFromConsole;
+    }
+
+    public static int readInt() {
+        int number;
+        while (true) {
+            try {
+                number = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return number;
+    }
 }
