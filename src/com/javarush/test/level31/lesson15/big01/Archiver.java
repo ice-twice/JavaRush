d7c83228623963a929aa2dee2dfc21285f64257d
awecea <awecea@gmail.com>
2016-11-22 22:05:26 +0200

    add step 8 big task level 31

--- src/com/javarush/test/level31/lesson15/big01/Archiver.java
+++ src/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -1,21 +1,36 @@
 package com.javarush.test.level31.lesson15.big01;
 
-import com.javarush.test.level31.lesson15.big01.command.ExitCommand;
+import com.javarush.test.level31.lesson15.big01.exception.WrongZipFileException;
+
+import java.io.IOException;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        /*try {
-            ConsoleHelper.writeMessage("Enter the path to create a new archive:");
-            String pathArchive = ConsoleHelper.readString();
-            ZipFileManager manager = new ZipFileManager(Paths.get(pathArchive));
-            ConsoleHelper.writeMessage("Enter the path to the file to be archived:");
-            String pathFile = ConsoleHelper.readString();
-            manager.createZip(Paths.get(pathFile));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+        Operation operation = null;
+        while (true) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+            if (operation == Operation.EXIT) {
+                break;
+            }
+        }
+    }
 
-        ExitCommand command = new ExitCommand();
-        command.execute();
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Select operations:");
+        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - " + Operation.CREATE.name());
+        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - " + Operation.ADD.name());
+        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - " + Operation.REMOVE.name());
+        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - " + Operation.EXTRACT.name());
+        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - " + Operation.CONTENT.name());
+        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - " + Operation.EXIT.name());
+        int operation = ConsoleHelper.readInt();
+        return Operation.values()[operation];
     }
 }
