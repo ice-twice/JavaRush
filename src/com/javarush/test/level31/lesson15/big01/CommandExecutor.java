dd71b15e1dbbaabf49bd69397b3b2c49909de60e
awecea <awecea@gmail.com>
2016-11-22 21:45:37 +0200

    add step 7 big task level 31

--- src/com/javarush/test/level31/lesson15/big01/CommandExecutor.java
+++ src/com/javarush/test/level31/lesson15/big01/CommandExecutor.java
@@ -6,17 +6,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommandExecutor {
-
-    private static final Map<Operation, Command> allKnownCommandsMap;
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
 
     static {
-        allKnownCommandsMap = new HashMap<>();
-        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
-        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
         allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
-        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
-        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
         allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
     }
 
     private CommandExecutor() {
