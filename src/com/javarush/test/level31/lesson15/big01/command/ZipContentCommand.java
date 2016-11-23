98bdfee45a79ebe0dbb5cc772483d76eac63507a
awecea <awecea@gmail.com>
2016-11-23 17:30:18 +0200

    add step 14 big task level 31

--- src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
@@ -1,8 +1,18 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.FileProperties;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        for (FileProperties fileProperties : zipFileManager.getFilesList()) {
+            ConsoleHelper.writeMessage(fileProperties.toString());
+        }
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
