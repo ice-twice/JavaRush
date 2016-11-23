37c595f40fd788f0da44fc0719dfb5f4a58c5574
awecea <awecea@gmail.com>
2016-11-23 17:18:11 +0200

    add step 13 big task level 31

--- src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -1,13 +1,17 @@
 package com.javarush.test.level31.lesson15.big01;
 
 import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundException;
+import com.javarush.test.level31.lesson15.big01.exception.WrongZipFileException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
@@ -68,5 +72,26 @@ public class ZipFileManager {
         while ((len = in.read(buffer)) > 0) {
             out.write(buffer, 0, len);
         }
+    }
+
+    public List<FileProperties> getFilesList() throws Exception {
+        if (!Files.isRegularFile(zipFile)) {
+            throw new WrongZipFileException();
+        }
+        List<FileProperties> fileProperties = new ArrayList<>();
+        try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                copyData(zipInputStream, byteArrayOutputStream);
+                String name = zipEntry.getName();
+                long size = byteArrayOutputStream.size();
+                long compressedSize = zipEntry.getCompressedSize();
+                int compressionMethod = zipEntry.getMethod();
+                FileProperties zipFileProperties = new FileProperties(name, size, compressedSize, compressionMethod);
+                fileProperties.add(zipFileProperties);
+            }
+        }
+        return fileProperties;
     }
 }
