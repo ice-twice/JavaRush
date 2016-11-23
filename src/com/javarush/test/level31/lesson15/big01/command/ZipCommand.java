9251bb87276a6dcec2f3dc6d5fcb23291257ae61
awecea <awecea@gmail.com>
2016-11-23 16:32:19 +0200

    add step 12 big task level 31

--- src/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipCommand.java
@@ -3,13 +3,14 @@ package com.javarush.test.level31.lesson15.big01.command;
 import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
 import com.javarush.test.level31.lesson15.big01.ZipFileManager;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public abstract class ZipCommand implements Command {
-    public ZipFileManager getZipFileManager() throws
-            Exception {
-        ConsoleHelper.writeMessage("Enter the path to the archive");
-        String path = ConsoleHelper.readString();
-        return new ZipFileManager(Paths.get(path));
+
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
