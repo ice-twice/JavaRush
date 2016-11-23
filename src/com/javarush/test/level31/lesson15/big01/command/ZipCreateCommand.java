1b17f6fdde1193e76f91906971e76a5d11df71b6
awecea <awecea@gmail.com>
2016-11-23 15:37:50 +0200

    add step 11 big task level 31

--- src/com/javarush/test/level31/lesson15/big01/command/ZipCreateCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipCreateCommand.java
@@ -1,8 +1,24 @@
 package com.javarush.test.level31.lesson15.big01.command;
 
+import com.javarush.test.level31.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level31.lesson15.big01.ZipFileManager;
+import com.javarush.test.level31.lesson15.big01.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Enter the path to the archiving.");
+            Path path = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(path);
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
