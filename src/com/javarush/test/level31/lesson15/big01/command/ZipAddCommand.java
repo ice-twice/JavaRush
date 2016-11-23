791e5d6adc563c84ed234af0d233498349211b96
awecea <awecea@gmail.com>
2016-11-23 23:46:25 +0200

    add step 18 big task level 31

--- src/com/javarush/test/level31/lesson15/big01/command/ZipAddCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipAddCommand.java
@@ -11,17 +11,19 @@ public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         try {
-            ConsoleHelper.writeMessage("Добавление файла в архив.");
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
 
             ZipFileManager zipFileManager = getZipFileManager();
 
-            ConsoleHelper.writeMessage("Введите полный путь добавляемого файла:");
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
             Path sourcePath = Paths.get(ConsoleHelper.readString());
+
             zipFileManager.addFile(sourcePath);
 
             ConsoleHelper.writeMessage("Добавление в архив завершено.");
+
         } catch (PathIsNotFoundException e) {
-            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+            ConsoleHelper.writeMessage("Файл не был найден.");
         }
     }
 }
