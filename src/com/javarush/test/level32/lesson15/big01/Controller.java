14c0c3840608ee73e33d479874f8930bc2957721
awecea <awecea@gmail.com>
2016-11-27 12:53:37 +0200

    add step 2 big task level 32

--- src/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -11,5 +11,21 @@ public class Controller {
     public Controller(View view) {
         this.view = view;
     }
+
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
+    }
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
 }
 
