828daeaf1d0919e41e00aec46ae07c0f02ae8a94
awecea <awecea@gmail.com>
2016-11-28 14:04:15 +0200

    add step 10 big task level 32

--- src/com/javarush/test/level32/lesson15/big01/listeners/UndoMenuListener.java
+++ src/com/javarush/test/level32/lesson15/big01/listeners/UndoMenuListener.java
@@ -20,7 +20,8 @@ public class UndoMenuListener implements MenuListener {
 
     @Override
     public void menuSelected(MenuEvent e) {
-
+        undoMenuItem.setEnabled(view.canUndo());
+        redoMenuItem.setEnabled(view.canRedo());
     }
 
     @Override
